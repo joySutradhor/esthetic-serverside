5ad0f3f606add5f38911b16adaacd94986f38985
joySutradhor <joysutradhorcmt@gmail.com>
2025-03-09 10:21:54 +0600

    cancel order

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,6 +1,6 @@
 import express from  "express"
 
-import { fetch , create , getOrders , updateOrder , deleteOrder , multipleOrders , getAcceptedOrder, getCancelOrder} from "../controller/oderController.js"
+import { fetch , create , getOrders , updateOrder , deleteOrder , multipleOrders , getAcceptedOrder, getCancelOrder, cancelOrder} from "../controller/oderController.js"
 
 const route = express.Router();
 
@@ -11,6 +11,7 @@ route.get("/orders/cancel" , getCancelOrder)
 route.get ("/orders/:phone" , multipleOrders)
 route.post("/create" , create)
 route.patch ("/update/:id" , updateOrder)
+route.patch ("/cancel/:id" , cancelOrder)
 route.delete ("/deleteOrder/:id" , deleteOrder)
 
 export default route ;
